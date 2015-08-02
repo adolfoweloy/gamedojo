598d327b76c6d55a035d7cf86023cb70b477ec88
aeloy <adolfo.eloy@gmail.com>
2015-08-02 01:31:12 -0300

    adiciona um teste com world matando um player

--- src/main/java/br/com/gamedojo/Main.java
+++ src/main/java/br/com/gamedojo/Main.java
@@ -5,6 +5,7 @@ import br.com.gamedojo.model.game.Game;
 import br.com.gamedojo.model.game.Match;
 import br.com.gamedojo.model.game.Weapon;
 import br.com.gamedojo.model.player.Player;
+import br.com.gamedojo.model.world.World;
 
 public class Main {
 
@@ -15,11 +16,13 @@ public class Main {
         Player roman = new Player("Roman");
         Player nick = new Player("Nick");
         Player fury = new Player("Fury");
+        World world = new World();
 
         match.addEvent(roman.kill(nick, Weapon.M16, parse("23/04/2013 15:36:04")));
         match.addEvent(roman.kill(fury, Weapon.M16, parse("23/04/2013 15:36:04")));
         match.addEvent(fury.kill(nick, Weapon.M16, parse("23/04/2013 15:36:04")));
         match.addEvent(roman.kill(nick, Weapon.M16, parse("23/04/2013 15:36:04")));
+        match.addEvent(world.kill(roman, Weapon.INVALID, parse("23/04/2013 15:36:04")));
 
         match.getStatistics().forEach(agent ->  {
             Player player = (Player) agent;
